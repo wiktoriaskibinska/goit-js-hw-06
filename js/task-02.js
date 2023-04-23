ccb19135f15a6bd459fdd1722957dd68f0b92543
Wiktoria Skibińska <wikas2000@icloud.com>
2023-04-23 18:56:47 +0200

    added second asessment

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+for (const ingr of ingredients) {
+  // Create the list element
+  const addItem = document.createElement("li");
+  addItem.classList.add("item");
+  addItem.textContent = ingr;
+  list.append(addItem);
+}
