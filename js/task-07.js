e9043b2787f284c35d073f6d5ef5cd920667504e
Wiktoria Skibińska <wikas2000@icloud.com>
2023-04-23 23:19:29 +0200

    task 7 done

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+input.addEventListener("input", () => {
+  text.style.fontSize = input.value + "px";
+});
