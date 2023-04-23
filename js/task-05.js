34a5b7ba44b74d1f1aae2ea1378ce2ffbcb9a06c
Wiktoria Skibińska <wikas2000@icloud.com>
2023-04-23 22:57:38 +0200

    different solution to task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,5 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
-//input.addEventListener("input", (event) => {
-//  output.textContent = event.currentTarget.value;
-//});
-
 input.addEventListener("input", () => {
   const name = input.value;
   if (name === "") {
@@ -12,3 +8,7 @@ input.addEventListener("input", () => {
     output.textContent = name;
   }
 });
+//OR
+//input.addEventListener("input", (event) => {
+//  output.textContent = event.currentTarget.value;
+//});
