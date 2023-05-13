bb24a1ed0af363716a53241901290fc6bb0a5f86
Wiktoria Skibińska <wikas2000@icloud.com>
2023-05-13 14:42:55 +0200

    uptade of task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form= document.querySelector(".login-form")
+form.addEventListener("submit", handler);
+function handler(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
 
+    if (email.value === "" || password.value === "") {
+    
+        return alert("Please fill in all the fields!");
+    }
+    else {
+        const user = {
+            email: email.value,
+            password: password.value,
+        }
+        console.log(user)
+    }
+ event.currentTarget.reset();
+}
