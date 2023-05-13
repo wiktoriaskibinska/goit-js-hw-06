286e6b26a389bde3918ed7a82fe220c244673694
Wiktoria Skibińska <wikas2000@icloud.com>
2023-05-13 15:04:50 +0200

    uptated task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span=document.querySelector(".color")
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  span.textContent=newColor
+ 
+}
+)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
