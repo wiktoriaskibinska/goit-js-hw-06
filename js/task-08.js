d77c3a63464153d9d4bc08eaf23ed739b6e63940
Wiktoria Skibińska <wikas2000@icloud.com>
2023-07-08 15:59:45 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,4 @@
-const form= document.querySelector(".login-form")
+const form=document.querySelector(".login-form")
 form.addEventListener("submit", handler);
 function handler(event) {
     event.preventDefault();
