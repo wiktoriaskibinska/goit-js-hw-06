d92fd65496f68138c4faa67c4ce53551df206108
Wiktoria Skibińska <wikas2000@icloud.com>
2023-10-06 14:50:36 +0200

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,42 @@
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector('input[type="number"]')
+const createBtn =controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+
+createBtn.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  
+  }
+  
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+} 
+
+
