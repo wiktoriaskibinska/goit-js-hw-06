d9009299848d5a6fde5205b2a74fa099495264b5
Wiktoria Skibińska <wikas2000@icloud.com>
2023-11-05 23:37:56 +0100

    remark task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,9 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 input.addEventListener("input", () => {
-  const name = input.value;
+  output.textContent = input.value;
+})
+ /* const name = input.value;
   
  if (name === null) {
     output.textContent === "Anonymous";
