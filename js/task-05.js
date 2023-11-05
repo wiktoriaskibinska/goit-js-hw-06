b0517d9c9765d153286f3b7a83c7151c3dcef7b6
Wiktoria Skibińska <wikas2000@icloud.com>
2023-11-05 23:33:17 +0100

    addjustments in task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,12 +2,16 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 input.addEventListener("input", () => {
   const name = input.value;
-  if (name === "") {
+  
+ if (name === null) {
     output.textContent === "Anonymous";
   } else {
     output.textContent = name;
   }
-});
+})
+//or
+ // output.textContent = input.value;
+//});
 //OR
 //input.addEventListener("input", (event) => {
 //  output.textContent = event.currentTarget.value;
